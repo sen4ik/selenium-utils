0600d0f02d34f9a7841ab54cbb5854f180674777
Artur Sentsov <sen4ik@gmail.com>
2021-02-09 22:54:37 -0800

    cleanup

--- src/test/java/org/sen4ik/utils/test/BaseTest.java
+++ src/test/java/org/sen4ik/utils/test/BaseTest.java
@@ -1,14 +1,12 @@
 package org.sen4ik.utils.test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.WebDriver;
 import org.sen4ik.utils.selenium.base.SeleniumUtils;
 import org.sen4ik.utils.selenium.driver.DriverFactory;
 import org.testng.annotations.*;
 
 @Slf4j
 public class BaseTest {
-
 
     @BeforeClass
     public void bc(){
